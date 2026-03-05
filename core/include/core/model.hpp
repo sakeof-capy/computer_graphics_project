b25b9a94803f86e8fb9bac8fb1656947b4d1f487
sakeof-capy <non-linear@ukr.net>
2026-03-05 23:56:19 +0200

    feat: hello-world project

---
+++ core/include/core/model.hpp
@@ -0,0 +1,23 @@
+#ifndef __MODEL_H__
+#define __MODEL_H__
+
+#include <vector>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+class Model {
+private:
+	std::vector<cv::Vec3f> verts_;
+	std::vector<std::vector<int> > faces_;
+public:
+	Model(const char *filename);
+	~Model();
+	int nverts();
+	int nfaces();
+	cv::Vec3f vert(int i);
+	std::vector<int> face(int idx);
+};
+
+#endif //__MODEL_H__
